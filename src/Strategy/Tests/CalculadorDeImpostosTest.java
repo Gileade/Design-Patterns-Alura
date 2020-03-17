7a0f6743b9a077a30d839ab70346c274dee52716
Gileade <gileade.trindade@gmail.com>
2020-03-17 17:40:20 -0300

    Ajustando Testes

--- src/Strategy/Tests/CalculadorDeImpostosTest.java
+++ src/Strategy/Tests/CalculadorDeImpostosTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class CalculadorDeImpostosTest {
 
+    //Testes ISS
     @Test
     public void deveRetornarSeisPorCentoDoOrcamentoISS(){
         Orcamento orcamento = new Orcamento(500);
@@ -16,6 +17,7 @@ public class CalculadorDeImpostosTest {
         Assert.assertEquals(30,calculadorDeImpostos.calcula(orcamento, iss),0.001);
     }
 
+    //Testes ICMS
     @Test
     public void deveRetornarCincoPorCentoDoOrcamentoMaisCinquentaICMS(){
         Orcamento orcamento = new Orcamento(500);
@@ -26,6 +28,7 @@ public class CalculadorDeImpostosTest {
         Assert.assertEquals(75,calculadorDeImpostos.calcula(orcamento, icms),0.001);
     }
 
+    //Testes ICCC
     @Test
     public void deveRetornarCincoPorcentoDoOrcamentoICCC(){
         Orcamento orcamento = new Orcamento(500);
@@ -34,19 +37,56 @@ public class CalculadorDeImpostosTest {
         CalculadorDeImpostos calculadorDeImpostos = new CalculadorDeImpostos();
 
         Assert.assertEquals(25,calculadorDeImpostos.calcula(orcamento, iccc ),0.001);
-
-
     }
+
     @Test
-    public void deveRetornarSetePorcentoDoOrcamentoICCC(){
+    public void deveRetornarCincoPorcentoDoOrcamentoICCC_MaiorFaixa(){
+        Orcamento orcamento = new Orcamento(999.99);
+        Imposto iccc = new ICCC();
+
+        CalculadorDeImpostos calculadorDeImpostos = new CalculadorDeImpostos();
+
+        Assert.assertEquals(49.999,calculadorDeImpostos.calcula(orcamento, iccc ),0.001);
+    }
+
+    @Test
+    public void deveRetornarSetePorcentoDoOrcamentoICCC_MenorFaixa(){
         Orcamento orcamento = new Orcamento(1000);
         Imposto iccc = new ICCC();
 
         CalculadorDeImpostos calculadorDeImpostos = new CalculadorDeImpostos();
 
         Assert.assertEquals(70,calculadorDeImpostos.calcula(orcamento, iccc ),0.001);
+    }
 
+    @Test
+    public void deveRetornarSetePorcentoDoOrcamentoICCC(){
+        Orcamento orcamento = new Orcamento(2000);
+        Imposto iccc = new ICCC();
 
+        CalculadorDeImpostos calculadorDeImpostos = new CalculadorDeImpostos();
+
+        Assert.assertEquals(140,calculadorDeImpostos.calcula(orcamento, iccc ),0.001);
+    }
+
+    @Test
+    public void deveRetornarSetePorcentoDoOrcamentoICCC_MaiorFaixa(){
+        Orcamento orcamento = new Orcamento(3000);
+        Imposto iccc = new ICCC();
+
+        CalculadorDeImpostos calculadorDeImpostos = new CalculadorDeImpostos();
+
+        Assert.assertEquals(210,calculadorDeImpostos.calcula(orcamento, iccc ),0.001);
+    }
+
+    @Test
+    public void deveRetornarOitoPorcentoMaisTrintaDoOrcamentoICCC_MenorFaixa(){
+        Orcamento orcamento = new Orcamento(3001);
+        Imposto iccc = new ICCC();
+
+        CalculadorDeImpostos calculadorDeImpostos = new CalculadorDeImpostos();
+
+        Assert.assertEquals(270.08,calculadorDeImpostos.calcula(orcamento, iccc ),0.001);
     }
 
     @Test
@@ -57,7 +97,5 @@ public class CalculadorDeImpostosTest {
         CalculadorDeImpostos calculadorDeImpostos = new CalculadorDeImpostos();
 
         Assert.assertEquals(830,calculadorDeImpostos.calcula(orcamento, iccc ),0.001);
-
-
     }
 }
